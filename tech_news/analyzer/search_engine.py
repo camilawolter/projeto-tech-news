06e14b788c465eae06dbc74f7c20978b4cf8f2fd
Camila Wolter <camila0wolter@gmail.com>
2023-04-13 18:37:56 -0300

    feat: criando a  a função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,18 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    news = search_news(query)
+
+    result = []
+    for new in news:
+        title = new["title"]
+        url = new["url"]
+        result.append((title, url))
+
+    return result
 
 
 # Requisito 8
